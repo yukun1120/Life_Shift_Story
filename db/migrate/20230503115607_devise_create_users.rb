75411986a7ff3272025c0af0d7edd11bf02a4c82
yuna1120 <kumakumabear1120@icloud.com>
2023-05-09 17:24:41 +0900

    deviseのuserのmigrate

--- db/migrate/20230503115607_devise_create_users.rb
+++ db/migrate/20230503115607_devise_create_users.rb
@@ -4,8 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, unique: true, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name, null: false
+      t.string :nickname, null: false
+      t.date :birthday, null: false
 
       ## Recoverable
       t.string   :reset_password_token
