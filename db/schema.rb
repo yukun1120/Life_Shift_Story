af7b82b5122d60e07e5b8f441c8e30aee52c36a2
yuna1120 <kumakumabear1120@icloud.com>
2023-05-15 18:12:34 +0900

    修正完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_13_063737) do
+ActiveRecord::Schema.define(version: 2023_05_15_090215) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2023_05_13_063737) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "confirmed", default: false
     t.index ["user_id"], name: "index_goods_on_user_id"
   end
 
