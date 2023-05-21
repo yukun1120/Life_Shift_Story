9d17761ee4ef3462f537f65aca42e82d5674f551
yuna1120 <kumakumabear1120@icloud.com>
2023-05-21 17:21:30 +0900

    b

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_19_090724) do
+ActiveRecord::Schema.define(version: 2023_05_20_075051) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
