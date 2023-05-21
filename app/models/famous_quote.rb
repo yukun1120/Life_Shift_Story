b9f9c26471f1298c745b9f9eb7920b2549502381
yuna1120 <kumakumabear1120@icloud.com>
2023-05-21 21:06:55 +0900

    RUND書き換え

--- app/models/famous_quote.rb
+++ app/models/famous_quote.rb
@@ -4,7 +4,7 @@ class FamousQuote < ApplicationRecord
 
   def self.update_random_quote_for_user(user)
     where(user: user).update_all(displayed: false)
-    random_quote = where(user: user).order("RANDOM()").first
+    random_quote = where(user: user).order("RAND()").first
     random_quote.update!(displayed: true) if random_quote.present?
   end
 end
