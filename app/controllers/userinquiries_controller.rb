276254432b822494d9403981d70c9ce54977d416
yuna1120 <kumakumabear1120@icloud.com>
2023-05-22 21:30:34 +0900

    完成

--- app/controllers/userinquiries_controller.rb
+++ app/controllers/userinquiries_controller.rb
@@ -2,4 +2,18 @@ class UserinquiriesController < ApplicationController
   def index
     @userinquiry = Userinquiry.new
   end
+
+  def create
+    @userinquiry = Userinquiry.new(userinquiry_params)
+    if @userinquiry.save
+      redirect_to homes_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def userinquiry_params
+    params.require(:userinquiry).permit(:title, :content).merge(user_id: current_user.id)
+  end
 end
