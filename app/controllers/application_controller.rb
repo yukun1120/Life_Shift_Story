3380fa606991b2140de3ae33e7bb0350d3675cfb
yuna1120 <kumakumabear1120@icloud.com>
2023-05-22 11:01:24 +0900

    Basic認証無効化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth
+  # before_action :basic_auth
 
 
 
@@ -10,11 +10,11 @@ class ApplicationController < ActionController::Base
   end
 
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+  #   end
+  # end
 
   protected
 
