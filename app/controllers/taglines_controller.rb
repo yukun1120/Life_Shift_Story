0a9903c5007feb6856363a7ddc7dd1d7740acf1e
yukun1120 <kumakumabear1120@icloud.com>
2023-06-18 23:24:44 +0900

    コントローラー１

--- app/controllers/taglines_controller.rb
+++ app/controllers/taglines_controller.rb
@@ -1,2 +1,14 @@
 class TaglinesController < ApplicationController
+  def index
+    @tagline = Tagline.new
+  end
+
+  def create
+    Tagline.create(tagline_params)
+  end
+
+  private
+  def tagline_params
+    params.require(:tagline).permit(:title, :content)
+  end
 end
