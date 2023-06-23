ffbd4a6a8096a6a8a79d6ed127069bf3e55c8a67
yukun1120 <kumakumabear1120@icloud.com>
2023-06-24 00:00:59 +0900

    update controller(add only edit yet)

--- app/controllers/taglines_controller.rb
+++ app/controllers/taglines_controller.rb
@@ -1,18 +1,49 @@
 class TaglinesController < ApplicationController
+  before_action :set_famous_quote, only: [:edit, :update, :destroy]
+
+#ここから再開
+
   def index
-    @tagline = Tagline.new
+    @famous_quotes = current_user.famous_quotes
+  end
+
+  def new
+    @famous_quote = FamousQuote.new
   end
 
   def create
-    @tagline = Tagline.create(tagline_params)
-    if @tagline.save
-      redirect_to homes_path
+    @famous_quote = FamousQuote.new(famous_quote_params)
+    if @famous_quote.valid?
+      @famous_quote.save
+      redirect_to famous_quotes_path
     else
-      render :index
+      
+      render :new
+
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @famous_quote.update(famous_quote_params)
+      redirect_to famous_quotes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @famous_quote.destroy
+    redirect_to famous_quotes_path
+  end
+
   private
+  def set_famous_quote
+    @famous_quote = FamousQuote.find(params[:id])
+  end
+
   def tagline_params
     params.require(:tagline).permit(:title, :content).merge(user_id: current_user.id)
   end
