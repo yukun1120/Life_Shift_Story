7e1b41973402002754246c138ee83ffa84f98d21
yukun1120 <kumakumabear1120@icloud.com>
2023-06-23 23:46:57 +0900

    モデル　追加

--- app/models/tagline.rb
+++ app/models/tagline.rb
@@ -1,4 +1,10 @@
 class Tagline < ApplicationRecord
   belongs_to :user
   validates :title, :content, presence: true
+
+  def self.update_random_tagline_for_user(user)
+    where(user: user).update_all(displayed: false)
+    random_tagline = where(user: user).order("RAND()").first
+    random_tagline.update!(displayed: true) if random_tagline.present?
+  end
 end
