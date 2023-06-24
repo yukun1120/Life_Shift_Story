de32aae8d6f7e9d3a0e05a4c19ac8a6e82e1be5c
yukun1120 <kumakumabear1120@icloud.com>
2023-06-24 23:52:09 +0900

    Update taglines_controller.rb

--- app/controllers/taglines_controller.rb
+++ app/controllers/taglines_controller.rb
@@ -1,47 +1,50 @@
 class TaglinesController < ApplicationController
-  before_action :set_famous_quote, only: [:edit, :update, :destroy]
+  before_action :set_tagline, only: [:edit, :update, :destroy]
 
 #ここから再開
 
   def index
-    @famous_quotes = current_user.famous_quotes
+    @taglines = current_user.taglines
   end
 
   def new
-    @famous_quote = FamousQuote.new
+    @tagline = Tagline.new
   end
 
   def create
-    @famous_quote = FamousQuote.new(famous_quote_params)
-    if @famous_quote.valid?
-      @famous_quote.save
-      redirect_to famous_quotes_path
+    @tagline = Tagline.new(tagline_params)
+    if @tagline.valid?
+      @tagline.save
+      redirect_to taglines_path
     else
       
       render :new
 
     end
   end
+  def show
+    @tagline = Tagline.find(params[:id])
+  end
 
   def edit
   end
 
   def update
-    if @famous_quote.update(famous_quote_params)
-      redirect_to famous_quotes_path
+    if @tagline.update(tagline_params)
+      redirect_to taglines_path
     else
       render :edit
     end
   end
 
   def destroy
-    @famous_quote.destroy
-    redirect_to famous_quotes_path
+    @tagline.destroy
+    redirect_to taglines_path
   end
 
   private
-  def set_famous_quote
-    @famous_quote = FamousQuote.find(params[:id])
+  def set_tagline
+    @tagline = Tagline.find(params[:id])
   end
 
   def tagline_params
